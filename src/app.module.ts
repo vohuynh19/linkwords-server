c82608bf0374a0ceff7d7ddfa86e967bdd6d13d7
VoHuynh <huynh.vo@unifiedpost.com>
2024-06-10 00:43:04 +0700

    feat: temp startup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 
 import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
-import { EventsModule } from './event.module';
+import { EventGateway } from './event.gateway';
+import { UserService } from './user/user.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [BookModule, UserModule],
   controllers: [AppController],
-  providers: [AppService, EventsModule],
+  providers: [AppService, PrismaService, UserService, EventGateway],
 })
 export class AppModule {}
--- src/event.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventGateway } from './event.gateway';
-import { UserModule } from './user/user.module';
-
-@Module({
-  imports: [UserModule],
-  providers: [EventGateway],
-})
-export class EventsModule {}
